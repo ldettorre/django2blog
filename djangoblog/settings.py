cb5ba56098a3ac8899769c7512c833de8bad2dd1
Luca DEttorre <ldettorre21@gmail.com>
2018-06-18 10:18:15 +0000

    added accounts

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y46cr)e_=gwou^_kp!t)m#-^!czf^)3#^lo%gq1!wy3-%687)k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangoblog-richardadalton.c9users.io']
+ALLOWED_HOSTS = ['django-blog-features-lucad.c9users.io']
 
 
 # Application definition
